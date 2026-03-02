9b9d666d3ce99c795c53492cf3824155ea9bc402
mariammohamedavir <mariam.mohamed.avir@gmail.com>
2026-03-02 14:07:47 +0200

    Refactor chat.py for improved readability and structure

--- chat.py
+++ chat.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import nltk
 import random
 import numpy as np
@@ -11,64 +5,101 @@ import json
 import pickle
 from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import load_model
-lemmatizer=WordNetLemmatizer()
+
+# ==============================
+# Download required NLTK data
+# ==============================
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('corpora/wordnet')
+except LookupError:
+    nltk.download('wordnet')
+
+# ==============================
+# Initialize Lemmatizer
+# ==============================
+
+lemmatizer = WordNetLemmatizer()
+
+# ==============================
+# Load Files
+# ==============================
 
 with open('breastCancer.json') as json_file:
     intents = json.load(json_file)
 
-words=pickle.load(open('words.pkl','rb'))
-classes=pickle.load(open('classes.pkl','rb'))
-model=load_model('chatbotmodel.h5')
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbotmodel.h5')
+
+# ==============================
+# Preprocessing Functions
+# ==============================
 
 def clean_up_sentence(sentence):
-  sentence_words=nltk.word_tokenize(sentence)
-  sentence_words=[lemmatizer.lemmatize(word) for word in sentence_words]
-  return sentence_words
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
 
 def bag_of_words(sentence):
-  sentence_words=clean_up_sentence(sentence)
-  bag=[0]*len(words)
-  for w in sentence_words:
-    for i,word in enumerate(words):
-      if word == w:
-        bag[i]=1
-  return np.array(bag)
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+
+    return np.array(bag)
+
+# ==============================
+# Prediction
+# ==============================
 
 def predict_class(sentence):
-  bow=bag_of_words(sentence)
-  res=model.predict(np.array([bow]))[0]
-  ERROR_THRESHOLD=0.25
-  results=[[i,r] for i,r in enumerate(res) if r> ERROR_THRESHOLD]
-  results.sort(key=lambda x:x[1],reverse=True)
-  return_list=[]
-  for r in results:
-    return_list.append({'intent': classes[r[0]],'probability':str(r[1])})
-  return return_list
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]), verbose=0)[0]
 
-def get_response(intents_list,intents_json):
-    result = None
-    tag=intents_list[0]['intent']
-    print(intents_list[0])
-    list_of_intents=intents_json['intents']
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+
+    return_list = []
+    for r in results:
+        return_list.append({
+            'intent': classes[r[0]],
+            'probability': str(r[1])
+        })
+
+    return return_list
+
+# ==============================
+# Get Response
+# ==============================
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+
     for i in list_of_intents:
         if tag in i['tags']:
-            result=i['responses']
-            break
-    return result
+            return random.choice(i['responses'])
 
+    return "I don't know about it"
 
-def main_(message:str):
-    while True:
-        ints=predict_class(message)
-        if len(ints) > 0:
-            res=get_response(ints,intents)
-            return res
-        else:
-            return "I Donot know about it"
+# ==============================
+# Main Function (For Streamlit)
+# ==============================
 
+def chatbot_response(message: str):
+    ints = predict_class(message)
 
-# In[ ]:
-
-
-
-
+    if len(ints) > 0:
+        return get_response(ints, intents)
+    else:
+        return "I don't know about it"
